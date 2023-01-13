f18746ac7b033addfc6ba87a39374e3952fb33a0
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-01-13 20:12:02 +0300

    Добавьте файлы проекта.

---
+++ AdoNetSample2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AdoNetSample3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRequest = new System.Windows.Forms.TextBox();
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRequest
+            // 
+            this.tbRequest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbRequest.Location = new System.Drawing.Point(13, 13);
+            this.tbRequest.Name = "tbRequest";
+            this.tbRequest.Size = new System.Drawing.Size(539, 26);
+            this.tbRequest.TabIndex = 0;
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(13, 45);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 29);
+            this.show.TabIndex = 1;
+            this.show.Text = "Fill";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(539, 222);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(477, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 314);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.tbRequest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRequest;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AdoNetSample2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+using System.Data.Common;
+using System.Threading;
+using System.Net.Http;
+
+namespace AdoNetSample3
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn=null;
+        SqlDataAdapter da = null;
+        DataSet set = null;
+        SqlCommandBuilder cmd = null;
+        string cs = "";
+
+        public object InputFileStream { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            conn.ConnectionString = cs;
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            try {
+                SqlConnection conn = new SqlConnection(cs);
+                set = new DataSet();
+                string sql = tbRequest.Text;
+                da = new SqlDataAdapter(sql, conn);
+                dataGridView1.DataSource = null;
+
+                cmd = new SqlCommandBuilder(da);
+                da.Fill(set, "mybook");
+
+                dataGridView1.DataSource = set.Tables["mybook"];
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                    
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da.Update(set, "mybook");
+        }
+    }
+}
